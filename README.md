55ccb54521f477a515b310f3be5fa3e30f6bf648
mowen <mowen@meili-inc.com>
2019-03-17 11:43:53 +0800

    fix: init program

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # koa-cli
 koa+typescript 脚手架
+
+主要做三个步骤：
+
+* 初始化package.json文件
+* 安装所需的包
+* 将所需的koa模板文件以及README文件复制到项目中
---
+++ src/server.ts
@@ -0,0 +1,6 @@
+/*
+ * @Author: Mowen 
+ * @Date: 2019-03-16 14:06:57 
+ * @Last Modified by: Mowen
+ * @Last Modified time: 2019-03-17 11:43:37
+ */
